985101ad235d0d5325793e0d721a2704dd223670
Carl Kenner <carl.kenner@gmail.com>
2017-02-21 22:48:10 +1030

    Add vr_autoSwitchControllers CVar

--- neo/sys/win32/win_input.cpp
+++ neo/sys/win32/win_input.cpp
@@ -1139,7 +1139,7 @@ int idJoystickWin32::PollInputEvents( int inputDeviceNum )
 			}
 #endif
 			// Carl: Update VR_USE_MOTION_CONTROLS if we've just started using the XBox controller
-			if (commonVr->VR_USE_MOTION_CONTROLS && (xis.Gamepad.wButtons || xis.Gamepad.bLeftTrigger > 64 || xis.Gamepad.bRightTrigger > 64
+			if (commonVr->VR_USE_MOTION_CONTROLS && vr_autoSwitchControllers.GetBool() && (xis.Gamepad.wButtons || xis.Gamepad.bLeftTrigger > 64 || xis.Gamepad.bRightTrigger > 64
 				|| abs(xis.Gamepad.sThumbLX) + abs(xis.Gamepad.sThumbLY) > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE || abs(xis.Gamepad.sThumbRX) + abs(xis.Gamepad.sThumbRY) > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE ) )
 			{
 				commonVr->VR_USE_MOTION_CONTROLS = false;
@@ -1584,7 +1584,7 @@ int idJoystickWin32::PollInputEvents( int inputDeviceNum )
 					if ( OVR_SUCCESS( ovr_GetInputState( commonVr->hmdSession, ovrControllerType_Touch, &inputState ) ) )
 					{
 						// Carl: Update VR_USE_MOTION_CONTROLS if we've just started using Touch
-						if( !commonVr->VR_USE_MOTION_CONTROLS && !vr_controllerStandard.GetInteger() && (
+						if( !commonVr->VR_USE_MOTION_CONTROLS  && vr_autoSwitchControllers.GetBool() && !vr_controllerStandard.GetInteger() && (
 							inputState.Buttons || inputState.HandTrigger[0] > 0.25f || inputState.HandTrigger[1] > 0.25f || inputState.IndexTrigger[0] > 0.25f || inputState.IndexTrigger[1] > 0.25f
 							|| (fabs(inputState.Thumbstick[0].x) + fabs(inputState.Thumbstick[0].y) > 0.5f) || (fabs(inputState.Thumbstick[1].x) + fabs(inputState.Thumbstick[1].y) > 0.5f)
 							|| (inputState.Touches & (ovrTouch_LButtonMask | ovrTouch_RButtonMask)) ) )
